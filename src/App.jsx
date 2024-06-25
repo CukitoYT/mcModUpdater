a5ae90399e747426ea2871aca69e03176929c992
TheCukitoDev <gabriel.diaz.aguilera@gmail.com>
2024-06-25 14:23:07 +0200

    Created the callAPI function and made it work properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
+async function getModFiles(
+	modID,
+	mcVersion,
+	modVersion,
+	loader = 'Fabric' || 'Forge' || 'Quilt',
+	loaderPlace = 'Client' || 'Server'
+) {
+	const options = {
+		method: 'GET',
+		url: `https://api.curseforge.com/v1/mods/${modID}/files`,
+		headers: {
+			Accept: 'application/json',
+			'x-api-key':
+				'$2a$10$vZraELhJUVjlB.f16ndNAujFMUPtAmNuQlSNDI14Ilbk5EANECG3e',
+		},
+	};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	try {
+		const response = await axios.request(options);
+		const files = response.data.data; // Asegúrate de que esto es un array
+		const file = files.find(
+			(item) =>
+				item.gameVersions.includes(mcVersion) &&
+				item.gameVersions.includes(loader) &&
+				item.gameVersions.includes(loaderPlace)
+		);
+		return file ? file.downloadUrl : 'No file found';
+	} catch (error) {
+		console.error(error);
+		return 'Error fetching data';
+	}
 }
 
-export default App
+function App() {
+	const [modFileUrl, setModFileUrl] = useState('');
+
+	useEffect(() => {
+		getModFiles(303119, '1.20.4', '1.0.0', 'Fabric', 'Client').then(
+			setModFileUrl
+		);
+	}, []);
+
+	return (
+		<>
+			<a
+				href={modFileUrl}
+				target="_blank"
+			>
+				<button>Ir al archivo</button>
+			</a>
+		</>
+	);
+}
+
+export default App;
